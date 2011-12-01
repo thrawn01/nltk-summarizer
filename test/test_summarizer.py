6d9c32d15d8899179350df5c450c74a18cce5e8b
Derrick J. Wippler <thrawn@gmail.com>
2011-12-01 16:16:26 +0000

    Tried split sentences with regex, doesn't work 2 well

--- test/test_summarizer.py
+++ test/test_summarizer.py
@@ -1,12 +1,12 @@
 
 import unittest
 import summarizer
+import re
 
 class TestSummarizer(unittest.TestCase):
-    
+
     def test_summarizer_simple(self):
-        news_article1 = """ A woman and four children were wounded and their apparent attacker killed himself in a mass shooting Wednesday, 
-                            said police in Bay City, Texas.
+        news_article1 = re.sub('\n', '', """ A woman and four children were wounded and their apparent attacker killed himself in a mass shooting Wednesday, said police in Bay City, Texas.
                             The four children were flown to various hospitals, their ages and conditions were not immediately known, 
                             said Lt. Andrew Lewis, spokesman with the Bay City police.
                             The female shooting victim was undergoing surgery Wednesday evening, Lewis said.
@@ -14,9 +14,13 @@ class TestSummarizer(unittest.TestCase):
                             Police responded to a call at 3:18 p.m. (4:18 p.m. ET) that a woman had been shot. When they arrived, they 
                             discovered the other victims and the apparent shooter, Lewis said.
                             The suspect, a male, had died at the scene after apparently shooting himself in the head, Lewis said.
-                            Detectives are working to determine the relationship of the suspected shooter and the victims, he said.  """
-       
+                            Detectives are working to determine the relationship of the suspected shooter and the victims, he said.  """)
+        result = re.split('([\w|\s|,|:]*?[\.\!\?])', news_article1)
+        print result
+        for line in result:
+            print line
+
         # Return 1 sentence from the article
-        self.assertEquals(summarizer.summarize(sentence=1, text=news_article1),'')
+        #self.assertEquals(summarizer.summarize(sentences=1, text=news_article1),'')
 
 
