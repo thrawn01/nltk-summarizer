9d2727dca585967312454df322d2af9b03118ef8
Derrick J. Wippler <thrawn@gmail.com>
2011-12-01 21:31:57 +0000

    Summarizer works as expected

--- summarizer/__init__.py
+++ summarizer/__init__.py
@@ -1,6 +1,67 @@
 
+from nltk.tokenize import RegexpTokenizer
+from nltk.probability import FreqDist 
+from nltk.corpus import stopwords
+from collections import defaultdict
+import nltk.data
+import re
 
-def summarize(sentence, text):
-    return ''
+sentence_tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
+word_tokenizer = RegexpTokenizer('\w+')
+
+
+def most_frequent_words(original_sentences, frequent_words, num_sentences):
+    # Lower the case to make matching simple
+    sentences_lowered = [sentence.lower() for sentence in original_sentences]
+    # create a map of sentences with the highest concentration of frequent words
+    sentences = defaultdict(int)
+    # For the most frequent words
+    for word in frequent_words:
+        # While looking through our lowercased sentences
+        for i in range(0, len(sentences_lowered)):
+            # If we find the word in the sentence
+            if word in sentences_lowered[i]:
+                # Note the count in the map
+                sentences[original_sentences[i]] += 1
+    # Swap the sentence with the count
+    sentences = dict(zip(sentences.values(), sentences.keys()))
+    # sort the keys so we get the greatest frequency of words first
+    keys = sorted(sentences.keys(), reverse=True)
+    # Return the top num_sentences of high frequency worded sentences
+    return [sentences[keys[i]] for i in range(0, num_sentences)]
+
+
+def get_context(needle, haystack, context):
+    # Locate the value in the original_sentences
+    for i in range(0, len(haystack)):
+        if haystack[i] == needle:
+            prefix = i - context
+            if prefix < 0: 
+                prefix = 0
+            # Return the number of context sentences asked for
+            return haystack[prefix:i+(context+1)]
+
+
+def summarize(num_sentences, context_lines, text):
+    result = {}
+    # Remove newlines from the text 
+    text = re.sub('\s*[\n|\r|\r\n]\s*', ' ', text)
+    # Separate all the words
+    base_words = [word.lower() for word in word_tokenizer.tokenize(text)]
+    # Remove non-nouns from the words collected
+    words = [word for word in base_words if word not in stopwords.words()]
+    # Use a frequency distribution to encode how often a word occurs 
+    frequencies = FreqDist(words)
+    # Now create a set of the most frequent words, limit top 100 words
+    frequent_words = [pair[0] for pair in frequencies.items()[:100]]
+    # Separate all the sentences
+    original_sentences = sentence_tokenizer.tokenize(text)
+    # Find the sentences with the most frequent words
+    high_freq_sentences = most_frequent_words(original_sentences, frequent_words, num_sentences)
+    # Create a map with the High Frequence Sentences as the key, and the context as the value
+    for sentence in high_freq_sentences:
+        # Get context for each of the high freq sentences
+        result[sentence] = get_context(sentence, original_sentences, context_lines)
+    return result
 
 
--- test/test_summarizer.py
+++ test/test_summarizer.py
@@ -1,12 +1,30 @@
 
 import unittest
 import summarizer
-import re
+import nltk.data
 
 class TestSummarizer(unittest.TestCase):
+    
+    def test_get_context(self):
+        self.assertEquals(summarizer.get_context( 3, (1, 2, 3, 4, 5 ), 2), (1, 2, 3, 4, 5 ))
+        self.assertEquals(summarizer.get_context( 1, (1, 2, 3, 4, 5 ), 2), (1, 2, 3 ))
+        self.assertEquals(summarizer.get_context( 5, (1, 2, 3, 4, 5 ), 2), (3, 4, 5 ))
+        self.assertEquals(summarizer.get_context( 3, (1, 2, 3, 4, 5 ), 0), (3,))
+        self.assertEquals(summarizer.get_context( 3, (1, 2, 3, 4, 5 ), 1), (2, 3, 4))
+
+
+    def test_most_frequent_words(self):
+        sentences = [ 'I jumped over the rainbow', 'I jumped over the box', 'I hopped over the fence', 'I love tipping over cows' ]
+        frequent_words = ['jumped', 'rainbow']
+
+        result = summarizer.most_frequent_words(sentences, frequent_words, 1)
+        self.assertEquals(result, ['I jumped over the rainbow'])
+        result = summarizer.most_frequent_words(sentences, frequent_words, 2)
+        self.assertEquals(result, ['I jumped over the rainbow', 'I jumped over the box'])
+
 
     def test_summarizer_simple(self):
-        news_article1 = re.sub('\n', '', """ A woman and four children were wounded and their apparent attacker killed himself in a mass shooting Wednesday, said police in Bay City, Texas.
+        news_article1 = """ A woman and four children were wounded and their apparent attacker killed himself in a mass shooting Wednesday in Bay City, Texas.
                             The four children were flown to various hospitals, their ages and conditions were not immediately known, 
                             said Lt. Andrew Lewis, spokesman with the Bay City police.
                             The female shooting victim was undergoing surgery Wednesday evening, Lewis said.
@@ -14,13 +32,19 @@ class TestSummarizer(unittest.TestCase):
                             Police responded to a call at 3:18 p.m. (4:18 p.m. ET) that a woman had been shot. When they arrived, they 
                             discovered the other victims and the apparent shooter, Lewis said.
                             The suspect, a male, had died at the scene after apparently shooting himself in the head, Lewis said.
-                            Detectives are working to determine the relationship of the suspected shooter and the victims, he said.  """)
-        result = re.split('([\w|\s|,|:]*?[\.\!\?])', news_article1)
-        print result
-        for line in result:
-            print line
+                            Detectives are working to determine the relationship of the suspected shooter and the victims, he said.  """
+
 
         # Return 1 sentence from the article
-        #self.assertEquals(summarizer.summarize(sentences=1, text=news_article1),'')
+        result = summarizer.summarize(num_sentences=1, context_lines=2, text=news_article1)
+        expected = {'The four children were flown to various hospitals, their ages and conditions were not immediately known, said Lt. Andrew Lewis,'\
+                    ' spokesman with the Bay City police.': 
+                    [' A woman and four children were wounded and their apparent attacker killed himself in a mass shooting Wednesday in Bay City, Texas.', 
+                     'The four children were flown to various hospitals, their ages and conditions were not immediately known, said Lt. Andrew Lewis,'\
+                     ' spokesman with the Bay City police.',
+                     'The female shooting victim was undergoing surgery Wednesday evening, Lewis said.', 'The shooting occurred at a residence in the city outside Houston.'
+                     ]
+                   }
+        self.assertEqual(result, expected)
 
 
